85d6f7f7e4512d26412db6ba740c6ec49238bfcf
ParthDesai2012 <parthdesai863gmail.com>
2025-10-12 15:55:36 +0400

    Submited acp for day 6, module 4

--- module_4/day_6/acp.py
+++ module_4/day_6/acp.py
@@ -1,1 +1,13 @@
-input = int(input("Enter a number: "))
+import random
+import string
+
+def generate_random_password(length=12):
+    characters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    password = ''.join(random.choice(characters) for i in range(length))
+    password_list = list(password)
+    random.shuffle(password_list)
+    shuffled_password = ''.join(password_list)
+    return shuffled_password
+
+random_password = generate_random_password()
+print(random_password)
